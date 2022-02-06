65414632da100af1865b6789ce8629aa16c2ac1c
Jason Allum <jallum@gmail.com>
2022-02-06 18:48:45 -0500

    50% faster

--- swift/11. Container With Most Water.swift
+++ swift/11. Container With Most Water.swift
@@ -4,8 +4,10 @@ class Solution {
         var y = height.count - 1
         var largestArea = 0
         while (x < y) {
-            largestArea = max(largestArea, (y - x) * min(height[x], height[y]))
-            if height[x] < height[y] {
+            let xh = height[x]
+            let yh = height[y]
+            largestArea = max(largestArea, (y - x) * min(xh, yh))
+            if xh < yh {
                 x += 1
             } else {
                 y -= 1
