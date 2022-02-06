a42ef046445d5a10713191fcf993d2001985455e
Jason Allum <jallum@gmail.com>
2022-02-06 18:46:08 -0500

    Create 7. Reverse Integer.swift

---
+++ swift/7. Reverse Integer.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var y = x
+        var z = 0
+        while y != 0 {
+            let digit = y % 10
+            z = z * 10 + (digit)
+            y /= 10
+            if z < Int32.min || z > Int32.max {
+                return 0
+            }
+        }
+        return z
+    }
+}
