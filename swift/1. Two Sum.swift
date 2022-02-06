81dc68e830fbc92292e26ca5be96bf6746669510
Jason Allum <jallum@gmail.com>
2022-02-06 18:38:29 -0500

    Tweaked

--- swift/1. Two Sum.swift
+++ swift/1. Two Sum.swift
@@ -1,7 +1,7 @@
 class Solution {
   func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     for (ix, x) in nums.enumerated() {
-      for (iy, y) in nums.enumerated() where ix != iy {
+      for (iy, y) in nums.enumerated() where ix < iy {
         if x + y == target {
           return [ix, iy];
         }
