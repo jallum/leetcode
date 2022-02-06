05f9b6209900a0ca8a0d64f9a262df3086188b4c
Jason Allum <jallum@gmail.com>
2022-02-06 18:33:04 -0500

    Reworked with Kadane's algo

--- swift/53. Maximum Subarray.swift
+++ swift/53. Maximum Subarray.swift
@@ -1,16 +1,13 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
         guard nums.count > 0 else {
-            return 0
+            return Int.min
         }
         var maxSum = Int.min
-        let m = nums.count
-        for i in 0 ..< m {
-            var sum = 0
-            for j in i ..< m {
-                sum += nums[j]
-                maxSum = max(sum, maxSum)
-            }
+        var sum = 0
+        for i in 0 ..< nums.count {
+            sum = max(nums[i], sum + nums[i])
+            maxSum = max(sum, maxSum)
         }
         return maxSum
     }
