78a81329dc13e50defd4a05f0b4b51dc04ca864a
Jason Allum <jallum@gmail.com>
2022-02-07 09:29:39 -0500

    Improved; Simplified

--- swift/88. Merge Sorted Array.swift
+++ swift/88. Merge Sorted Array.swift
@@ -1,29 +1,17 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        guard nums2.count > 0 else {
-            return
-        }
-        var i = nums1.count - 1
+        var i = m + n - 1
         var j = m - 1
-        var jv = j < 0 ? Int.min : nums1[j]
         var k = n - 1
-        var kv = k < 0 ? Int.min : nums2[k]
-        repeat {
-            while i >= 0 && jv >= kv {
-                nums1[i] = jv
+        while i >= 0 && k >= 0 {
+            if j >= 0 && nums1[j] > nums2[k] {
+                nums1[i] = nums1[j]
                 j -= 1
-                jv = j < 0 ? Int.min : nums1[j]
-
-                i -= 1
-            }
-            while i >= 0 && jv <= kv {
-                nums1[i] = kv
+            } else {
+                nums1[i] = nums2[k]
                 k -= 1
-                print(k)
-                kv = k < 0 ? Int.min : nums2[k]
-                
-                i -= 1
             }
-        } while i >= 0
+            i -= 1
+        }
     }
 } 
