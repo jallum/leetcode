0a6d3f5d9ee9d05629e7ec0d17cc494b4a3b4b65
Jason Allum <jallum@gmail.com>
2022-02-07 09:53:09 -0500

    Improved; Simplified

--- swift/283. Move Zeroes.swift
+++ swift/283. Move Zeroes.swift
@@ -1,13 +1,9 @@
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
-        var i = 0
         var j = 0
-        while i < nums.count {
-            if nums[i] != 0 {
-                nums[j] = nums[i]
-                j += 1
-            }
-            i += 1
+        for n in nums where n != 0 {
+            nums[j] = n
+            j += 1
         }
         while j < nums.count {
             nums[j] = 0
