a830d95fd41b319616669fbbcc2e96a7fc069fbd
Jason Allum <jallum@gmail.com>
2022-02-08 08:07:32 -0500

    Improved

--- swift/121. Best Time to Buy and Sell Stock.swift
+++ swift/121. Best Time to Buy and Sell Stock.swift
@@ -1,10 +1,13 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
+        var minPrice = Int.max
         var maxProfit = 0
-        for i in 0 ..< prices.count {
-            for j in (i+1) ..< prices.count {
-                maxProfit = max(prices[j] - prices[i], maxProfit)
-            }
+        for p in prices {
+            if p < minPrice {
+                minPrice = p
+            } else if (p - minPrice) > maxProfit {
+                maxProfit = p - minPrice
+            } 
         }
         return maxProfit
     }
