0a1358b67ebc222260755a6d39de070b50651d5e
Jason Allum <jallum@gmail.com>
2022-02-09 08:57:01 -0500

    Update 118. Pascal's Triangle.swift

--- swift/118. Pascal's Triangle.swift
+++ swift/118. Pascal's Triangle.swift
@@ -1,14 +1,17 @@
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
-        var triangle : [[Int]] = [[1]]
-
+        var lastRow = [1]
+        var triangle = [lastRow]
+        
         for i in 1 ..< numRows {
-            let prow = triangle[i-1]
             var row = [Int](repeating:0, count: i+1)
-            for j in 0 ... i {
-                row[j] = (j > 0 && j < i) ? (prow[j-1] + prow[j]) : 1
+            row[0] = 1
+            row[i] = 1
+            for j in 1 ..< i {
+                row[j] = lastRow[j-1] + lastRow[j]
             }
             triangle.append(row)
+            lastRow = row
         }
         
         return triangle
