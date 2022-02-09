7d1d84de33f69f09b451c27573b082588551b14e
Jason Allum <jallum@gmail.com>
2022-02-09 10:18:52 -0500

    Update 733. Flood Fill.swift

--- swift/733. Flood Fill.swift
+++ swift/733. Flood Fill.swift
@@ -1,25 +1,26 @@
 class Solution {
     func floodFill(_ image: [[Int]], _ sr: Int, _ sc: Int, _ newColor: Int) -> [[Int]] {
+        let startColor = image[sr][sc]
+        guard startColor != newColor else {
+            return image
+        }
+
         var image = image
-        let mx = image.count
-        let my = image[0].count
-        var startColor = image[sr][sc]
-        
+        let mx = image.count - 1
+        let my = image[0].count - 1
+
         func df(_ x: Int, _ y: Int) {
-            guard x > -1 && x < mx, y > -1 && y < my, image[x][y] == startColor else {
+            guard image[x][y] == startColor else {
                 return
             }
-
             image[x][y] = newColor
-            df(x-1, y)
-            df(x+1, y)
-            df(x, y-1)
-            df(x, y+1)
+            if x > 0  { df(x-1, y) }
+            if x < mx { df(x+1, y) }
+            if y > 0  { df(x, y-1) }
+            if y < my { df(x, y+1) }
         }
 
-        if startColor != newColor {
-            df(sr, sc)
-        }
+        df(sr, sc)
         
         return image
     }
