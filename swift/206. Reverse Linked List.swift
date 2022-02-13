3b414901c604d3154b1322d0558a9c076d66d171
Jason Allum <jallum@gmail.com>
2022-02-13 17:49:08 -0500

    Update 206. Reverse Linked List.swift

--- swift/206. Reverse Linked List.swift
+++ swift/206. Reverse Linked List.swift
@@ -10,23 +10,22 @@
  */
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-        var newHead: ListNode? = nil
-
-        func _reverseList(_ node: ListNode?) -> ListNode? {
-            guard let node = node else {
-                return nil
-            }
-            guard let next = node.next else {
-                newHead = node
-                return node
-            }
-            let node1 = _reverseList(next)
-            node1?.next = node
-            node.next = nil
-            return node
+        guard var newHead = head else {
+            return nil
         }
-
-        _reverseList(head)
+        
+        func reverse(_ n: ListNode) -> ListNode? {
+            guard let nn = n.next else {
+                newHead = n
+                return n
+            }
+            let n1 = reverse(nn)
+            n1?.next = n
+            n.next = nil
+            return n
+        }
+        
+        reverse(newHead)
         return newHead
     }
 }
